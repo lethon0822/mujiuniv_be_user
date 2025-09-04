de2b5c299cc56c008aa8781ceb2f0290d9754b19
1ethon0822 <1ethyjj9240@gmail.com>
2025-09-04 11:07:02 +0900

    user

---
+++ src/main/java/com/green/muziuniv_be_user/MuziunivBeUserApplication.java
@@ -0,0 +1,13 @@
+package com.green.muziuniv_be_user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuziunivBeUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MuziunivBeUserApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/green/muziuniv_be_user/MuziunivBeUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.green.muziuniv_be_user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuziunivBeUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
