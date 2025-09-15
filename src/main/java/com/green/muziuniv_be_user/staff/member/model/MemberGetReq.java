0136b93bb711286f64ac180a6cf40e5ca901f5f8
1ethon0822 <1ethyjj9240@gmail.com>
2025-09-15 10:42:36 +0900

    구성원 현황 완료

---
+++ src/main/java/com/green/muziuniv_be_user/staff/member/model/MemberGetReq.java
@@ -0,0 +1,22 @@
+package com.green.muziuniv_be_user.staff.member.model;
+
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+
+public class MemberGetReq {
+    private String userRole;        // student | professor
+    private Integer deptId;
+    private Integer grade;          // 학생 전용
+    private String status;          // 공통
+    private String gender;          // 교수 전용 (user.gender 기준이면 학생도 사용 가능)
+    private String q;
+    private String searchBy;        // all | name | loginId | email
+}
+
+
