0ec9b9ae208129082c7f9c3b89499419b49e2c25
minbonjwa <minbonjwa@gmail.com>
2025-09-30 15:21:19 +0900

    휴보크직

--- src/main/java/com/green/muziuniv_be_user/application/user/Repository/ProfessorRepository.java
+++ src/main/java/com/green/muziuniv_be_user/application/user/Repository/ProfessorRepository.java
@@ -3,6 +3,10 @@ package com.green.muziuniv_be_user.application.user.Repository;
 import com.green.muziuniv_be_user.entity.professor.Professor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 
 public interface ProfessorRepository extends JpaRepository<Professor, Long> {
+
+    Optional<Professor> findByUserId(Long userId);
 }
