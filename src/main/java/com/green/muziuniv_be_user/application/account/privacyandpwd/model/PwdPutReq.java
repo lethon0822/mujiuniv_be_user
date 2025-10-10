25a83e416dc23599986e9f2c1070aa2a70532c4f
1ethon0822 <1ethyjj9240@gmail.com>
2025-10-10 12:45:34 +0900

    비밀번호 validㅁ 걸기

--- src/main/java/com/green/muziuniv_be_user/application/account/privacyandpwd/model/PwdPutReq.java
+++ src/main/java/com/green/muziuniv_be_user/application/account/privacyandpwd/model/PwdPutReq.java
@@ -13,6 +13,6 @@ public class PwdPutReq {
     private String email;
 
     @NotBlank(message = "비밀번호는 필수 입력 값입니다.")
-    @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,16}", message = "비밀번호는 8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.")
+    @Pattern(regexp="(?=.*[0-9])(?=.*[a-zA-Z]).{8,20}")
     private String password;
 }
