43a1082b4aa2b0b27f0b07a032b0b5e8e7f457fa
minbonjwa <minbonjwa@gmail.com>
2025-10-13 12:45:02 +0900

    휴복직 신청승인 완료

--- src/main/java/com/green/muziuniv_be_user/application/user/model/UserInfoGetDto.java
+++ src/main/java/com/green/muziuniv_be_user/application/user/model/UserInfoGetDto.java
@@ -1,12 +1,19 @@
 package com.green.muziuniv_be_user.application.user.model;
 
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserInfoGetDto {
     private Long userId;
     private String userName;
     private String deptName;
-    private Integer status;
+    private String userRole;   // ✅ 이거 추가 (PROFESSOR / STUDENT 구분용)
+    private String status;     // 혹시 상태값도 넘겨줄 수 있으면 같이 유지
 }
